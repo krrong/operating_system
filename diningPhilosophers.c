2a2742ac6b92357165d4c449e9b8ab0d58be8960
krrong <84285337+krrong@users.noreply.github.com>
2022-03-22 15:08:58 +0900

    Add files via upload

--- diningPhilosophers.c
+++ diningPhilosophers.c
@@ -3,51 +3,56 @@
 #include <semaphore.h>
 
 #define NUM 5
-sem_t forks[NUM];
+sem_t forks[NUM];  // forks
 
 
-void pickup_fork(int philosopher_num){
+void pickup(int philosopher_num){
 	sem_wait(&forks[philosopher_num % NUM]);
 }
 
-void return_fork(int philosopher_num){
+void putdown(int philosopher_num){
 	sem_post(&forks[philosopher_num % NUM]);
-}
-
-void *philosopher(void *arg){
-	int philosopher_num;
-	philosopher_num = (int) arg;
-
-	// pickup left fork
-	pickup_fork(philosopher_num);
-	printf("philosopher %d picks up the fork %d.\n", philosopher_num, philosopher_num);
-
-	// pickup right fork
-	pickup_fork(philosopher_num + 1);
-	printf("philosopher %d picks up the fork %d.\n", philosopher_num, (philosopher_num + 1) % NUM);
-
-	eating(philosopher_num);
-
-	// putdown right fork
-	return_fork(philosopher_num + 1);
-	printf("philosopher %d put down the fork %d.\n", philosopher_num, (philosopher_num + 1) % NUM);
-
-	// putdown left fork
-	return_fork(philosopher_num);
-	printf("philosopher %d put down the fork %d.\n", philosopher_num, philosopher_num);
-	
-	thinking(philosopher_num);
-
-	return NULL;
 }
 
 void thinking(int philosopher_num){
 	printf("%d is thinking\n", philosopher_num);
+
+	return;
 }
 
 void eating(int philosopher_num){
 	printf("%d is eating\n", philosopher_num);
+
+	return;
 }
+
+void *philosopher(void *arg){
+        int philosopher_num;
+        philosopher_num = (unsigned long int) arg;
+
+        // pickup left fork
+        pickup(philosopher_num);
+        printf("philosopher %d picks up the fork %d.\n", philosopher_num, philosopher_num);
+
+        // pickup right fork
+        pickup(philosopher_num + 1);
+        printf("philosopher %d picks up the fork %d.\n", philosopher_num, (philosopher_num + 1) % NUM);
+
+        eating(philosopher_num);
+
+        // putdown right fork
+        putdown(philosopher_num + 1);
+        printf("philosopher %d puts down the fork %d.\n", philosopher_num, (philosopher_num + 1) % NUM);
+
+        // putdown left fork
+        putdown(philosopher_num);
+        printf("philosopher %d puts down the fork %d.\n", philosopher_num, philosopher_num);
+
+        thinking(philosopher_num);
+
+        return NULL;
+}
+
 
 int main(){
 	pthread_t threads[NUM];
@@ -56,7 +61,7 @@ int main(){
 		sem_init(&forks[i], 0, 1);
 	}
 
-	for(int i=0; i<NUM; i++){
+	for(unsigned long int i=0; i<NUM; i++){
 		pthread_create(&threads[i], NULL, philosopher, (void*)i);
 	}
 	
